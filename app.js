d34df1fdd2fcae8c55309a111ac5bb703b3fb38c
marioosh <marioosh.net@gmail.com>
2014-04-01 12:49:54 +0200

    root

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var config = {
 }
 var server = new Hapi.Server('0.0.0.0', config.port, {cors: true});
 console.log(__dirname);
-server.route({method: 'GET', path: '/static/{path*}', handler: {directory: {path: __dirname+'/static'}}});
+server.route([
+    {method: 'GET', path: '/static/{path*}', handler: {directory: {path: __dirname+'/static'}}},
+    {method: 'GET', path: '/', handler: function(rq,rp){rp('<a href="/">home</a><br/><a href="static/css/main.css">main.css - server</a><br/><a href="static2/css/main.css">main.css - plugin</a><br/>');}},
+]);
 
 server.pack.require('./main', { config: config }, function (err) {
     if(err) {
--- main/lib/index.js
+++ main/lib/index.js
@@ -2,7 +2,7 @@ console.log(__dirname);
 exports.register = function(plugin, options, next) {
 	plugin.route([
         {method: 'GET', path: '/static2/{path*}', handler: {directory: {path: __dirname+'/static'}}},
-        {method: 'GET', path: '/static4/{path*}', handler: {directory: {path: '/lib/static'}}} // Ok on Win32
+        {method: 'GET', path: '/static3/{path*}', handler: {directory: {path: '/lib/static'}}} // Ok on Win32
     ]);
 	return next();
 }
