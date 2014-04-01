307d5118068ee71bdf0b99e2f6144e1bc4d02574
marioosh <marioosh.net@gmail.com>
2014-04-01 12:37:24 +0200

    routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var config = {
     port: 3001
 }
 var server = new Hapi.Server('0.0.0.0', config.port, {cors: true});
+console.log(__dirname);
 server.route({method: 'GET', path: '/static/{path*}', handler: {directory: {path: __dirname+'/static'}}});
 
 server.pack.require('./main', { config: config }, function (err) {
--- main/lib/index.js
+++ main/lib/index.js
@@ -1,7 +1,8 @@
+console.log(__dirname);
 exports.register = function(plugin, options, next) {
 	plugin.route([
-        {method: 'GET', path: '/static2/{path*}', handler: {directory: {path: __dirname+'/static'}}}
+        {method: 'GET', path: '/static2/{path*}', handler: {directory: {path: __dirname+'/static'}}},
+        {method: 'GET', path: '/static4/{path*}', handler: {directory: {path: '/lib/static'}}} // Ok on Win32
     ]);
 	return next();
 }
-
